29146bd5e6cd9ffd08e937899f20955a3c01ce54
Jess Pagan <jespagan@gmail.com>
2019-04-17 03:21:50 -0700

    Fix branch

--- src/components/courses/courses-page.js
+++ src/components/courses/courses-page.js
@@ -1,11 +1,33 @@
 import React, { Prototype } from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: ''
+      }
+    };
+  }
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
   render() {
     return (
-      <div>
+      <form>
         <h1>Courses</h1>
-      </div>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="Submit" value="Save" />
+      </form>
     );
   }
 }

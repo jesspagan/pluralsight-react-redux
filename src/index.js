0fbedbebe7da05f47daeab03199414f5a0e602dc
Jess Pagan <jespagan@gmail.com>
2019-04-17 23:08:07 -0700

    Squashed commit of the following:
    commit 1681ad7fdb372498d68cc5e6ad78c764c285dc34
    Author: Jess Pagan <jespagan@gmail.com>
    Date:   Wed Apr 17 23:05:51 2019 -0700
    
        Set development environment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
-import './styles/styles.css'; //Webpack can import CSS files too!
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-render(
-  <Router history={browserHistory} routes={routes} />,
-  document.getElementById('app')
-);
+function Hi() {
+  return <p>Hi.</p>;
+}
+
+render(<Hi />, document.getElementById('app'));
